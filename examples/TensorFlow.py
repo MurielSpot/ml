95f349a5d31fa0ca5c0470c8d80c65646ca331a5
Muriel <17842868+MurielSpot@users.noreply.github.com>
2018-08-14 22:17:23 +0800

    Update TensorFlow.py

--- examples/TensorFlow.py
+++ examples/TensorFlow.py
@@ -489,13 +489,123 @@ with tf.Session() as sess:
         if i%5==0:
             print(compute_accuracy(mnist.test.images,mnist.test.labels))
 
-
-
 # example 12 #########################################################
+# 训练集，测试集
+import tensorflow as tf
+from sklearn.datasets import load_digits
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import LabelBinarizer
 
+# load data.
+digits=load_digits()
+X=digits.data#从0到9的图片数据。
+y=digits.target
+y=LabelBinarizer().fit_transform(y)#把y变成二进制形式。
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=.3)
+
+def add_layer(inputs,in_size,out_size,layer_name,activation_function=None):
+    Weights=tf.Variable(tf.random_normal([in_size,out_size]))
+    biases=tf.Variable(tf.zeros([1,out_size])+0.1)
+    Wx_plus_b=tf.matmul(inputs,Weights)+biases
+    if activation_function is None:
+        outputs=Wx_plus_b
+    else:
+        outputs=activation_function(Wx_plus_b,)
+    #不加下面这句好像会报错。
+    tf.summary.histogram(layer_name+'/outputs',outputs)
+    return outputs
+
+# define placeholder for inputs to network
+xs=tf.placeholder(tf.float32,[None,64])#8*8
+ys=tf.placeholder(tf.float32,[None,10])
+
+# add output layer
+l1=add_layer(xs,64,100,'l1',activation_function=tf.nn.tanh)
+prediction=add_layer(l1,100,10,'l2',activation_function=tf.nn.softmax)
+
+# the error between prediction and real data.
+cross_entropy=tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction),reduction_indices=[1]))
+tf.summary.scalar('loss',cross_entropy)
+train_step=tf.train.GradientDescentOptimizer(0.6).minimize(cross_entropy)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    merged=tf.summary.merge_all()
+
+    # 分别记录train和test的summary。
+    train_writer=tf.summary.FileWriter('logs/train',sess.graph)
+    test_writer=tf.summary.FileWriter('logs/test',sess.graph)
+
+    for i in range(100):
+        sess.run(train_step,feed_dict={xs:X_train,ys:y_train})
+        if i%5==0:
+            #record loss
+            train_result=sess.run(merged,feed_dict={xs:X_train,ys:y_train})
+            test_result=sess.run(merged,feed_dict={xs:X_train,ys:y_train})
+            train_writer.add_summary(train_result,i)
+            test_writer.add_summary(test_result,i)
+
+# 可以用tensorboard直接看logs里的两个文件夹test，train里的图像。
 
 # example 13 #########################################################
+# dropout
 
+import tensorflow as tf
+from sklearn.datasets import load_digits
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+
+# load data.
+digits=load_digits()
+X=digits.data#从0到9的图片数据。
+y=digits.target
+y=LabelBinarizer().fit_transform(y)#把y变成二进制形式。
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=.3)
+
+def add_layer(inputs,in_size,out_size,layer_name,activation_function=None):
+    Weights=tf.Variable(tf.random_normal([in_size,out_size]))
+    biases=tf.Variable(tf.zeros([1,out_size])+0.1)
+    Wx_plus_b=tf.matmul(inputs,Weights)+biases
+    # dropout加在这里。
+    Wx_plus_b=tf.nn.dropout(Wx_plus_b,keep_prob)
+    if activation_function is None:
+        outputs=Wx_plus_b
+    else:
+        outputs=activation_function(Wx_plus_b,)
+    #不加下面这句好像会报错。
+    tf.summary.histogram(layer_name+'/outputs',outputs)
+    return outputs
+
+# define placeholder for inputs to network
+keep_prob=tf.placeholder(tf.float32)#结果不被dropout的概率。
+xs=tf.placeholder(tf.float32,[None,64])#8*8
+ys=tf.placeholder(tf.float32,[None,10])
+
+# add output layer
+l1=add_layer(xs,64,50,'l1',activation_function=tf.nn.tanh)
+prediction=add_layer(l1,50,10,'l2',activation_function=tf.nn.softmax)
+
+# the error between prediction and real data.
+cross_entropy=tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction),reduction_indices=[1]))
+tf.summary.scalar('loss',cross_entropy)
+train_step=tf.train.GradientDescentOptimizer(0.6).minimize(cross_entropy)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    merged=tf.summary.merge_all()
+
+    # 分别记录train和test的summary。
+    train_writer=tf.summary.FileWriter('logs/train',sess.graph)
+    test_writer=tf.summary.FileWriter('logs/test',sess.graph)
+
+    for i in range(100):
+        sess.run(train_step,feed_dict={xs:X_train,ys:y_train,keep_prob:0.5})
+        if i%5==0:
+            #record loss
+            train_result=sess.run(merged,feed_dict={xs:X_train,ys:y_train,keep_prob:1})
+            test_result=sess.run(merged,feed_dict={xs:X_train,ys:y_train,keep_prob:1 })
+            train_writer.add_summary(train_result,i)
+            test_writer.add_summary(test_result,i)
 
 # example 14 #########################################################
 
