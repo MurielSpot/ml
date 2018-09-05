64d7d83808da1cc215ecfd380a954ae040b07d55
Muriel <17842868+MurielSpot@users.noreply.github.com>
2018-09-05 20:02:32 +0800

    Update TensorFlow_11_20.py

--- examples/TensorFlow_11_20.py
+++ examples/TensorFlow_11_20.py
@@ -8,7 +8,7 @@
 17:自加1程序。
 18:fetch。
 19:一层隐藏层的神经网络（识别手写数字）。
-20:
+20:rnn，lstm rnn。
 '''
 
 # example 11 #########################################################
@@ -374,7 +374,104 @@ with tf.Session() as sess:
         print("epoch [%d]"%epoch," accuracy:",sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels}))
 
 # example 20 #########################################################
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
+datasets_path=r"./ml_datasets/MNIST_data/"
+mnist=input_data.read_data_sets(datasets_path,one_hot=True)
+
+# hyperparameters
+lr=0.001
+training_iters=10000
+batch_size=128
+#display_step=10
+
+n_inputs=28 # mnist data,img shape:28*28.每次输入一行.
+n_steps=28 # time steps.28行pixel.
+n_hidden_units=128
+n_classes=10 # mnist classes(0-9 digits)
+
+x=tf.placeholder(tf.float32,[None,n_steps,n_inputs])
+y=tf.placeholder(tf.float32,[None,n_classes])
+
+weights={
+    # (28,128)
+    "in":tf.Variable(tf.random_normal([n_inputs,n_hidden_units])),
+    # (128,10)
+    "out":tf.Variable(tf.random_normal([n_hidden_units,n_classes]))
+    }
+
+biases={
+    "in":tf.Variable(tf.constant(0.1,shape=[n_hidden_units])),
+    "out":tf.Variable(tf.constant(0.1,shape=[n_classes]))
+    }
+
+def RNN(X,weights,baises):
+    # hidden layer for input to cell.
+    # X(128 batch, 28 steps(行), 28 inputs(每行28个像素).
+    # X => (128*28,28 inputs).
+    X=tf.reshape(X,[-1,n_inputs])
+    X_in=tf.matmul(X,weights["in"])+biases["in"]
+    # X => (128 batch,28 steps,28 inputs).
+    X_in=tf.reshape(X_in,[-1,n_steps,n_hidden_units])
+
+    # cell.
+    lstm_cell=tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units,forget_bias=1.0,state_is_tuple=True)
+    # lstm_cell is divided into two parts (c_state,m_state).
+    _init_state=lstm_cell.zero_state(batch_size,dtype=tf.float32)
+    outputs,states=tf.nn.dynamic_rnn(lstm_cell,X_in,initial_state=_init_state,time_major=False)
+
+    # hidden layer for output as the final results.
+    # states[1]对应上面m_state.
+    results=tf.matmul(states[1],weights["out"])+biases["out"]
+    '''
+    #第二种获得结果的方法:
+    #unpack to list [(batch,outputs)..]*steps.
+    
+    outputs=tf.unpack(tf.transpose(outputs,[1,0,2])))#states is the last outputs.
+    results=tf.matmul(outputs[-1],weights["out"]+biases["out"]
+    '''
+    return results
+    
+pred=RNN(x,weights,biases)
+cost=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred,labels=y))
+train_op=tf.train.AdamOptimizer(lr).minimize(cost)
+
+correct_pred=tf.equal(tf.argmax(pred,1),tf.argmax(y,1))
+accuracy=tf.reduce_mean(tf.cast(correct_pred,tf.float32))
+
+init=tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    step=0
+    while step*batch_size<training_iters:
+        batch_xs,batch_ys=mnist.train.next_batch(batch_size)
+        batch_xs=batch_xs.reshape([batch_size,n_steps,n_inputs])
+        sess.run([train_op],feed_dict={
+            x:batch_xs,
+            y:batch_ys,
+            })
+        if step%10==0:
+            print(sess.run(accuracy,feed_dict={
+                x:batch_xs,
+                y:batch_ys
+                }))
+        step+=1
+    print("end")
+
+'''
+结果：
+0.2890625
+0.578125
+0.6875
+0.7734375
+0.7890625
+0.84375
+0.8203125
+0.8828125
+end
+'''
 
 # example 21 #########################################################
 
