98fa2a74139a898fa7c640758140ce182c4bbb57
Muriel <17842868+MurielSpot@users.noreply.github.com>
2018-09-07 17:26:15 +0800

    Update TensorFlow_21_25.py

--- examples/TensorFlow_21_25.py
+++ examples/TensorFlow_21_25.py
@@ -204,7 +204,8 @@ def decoder(x):
     注意这里：
     decoder函数开头的layers=[]，layers.append(x)应该与encoder函数里的return layers[-1]搭配。
     或者decoder函数开头的layers=[]，layers.append(x)改为layers=x,此时应该与encoder函数里的return layers搭配。
-    除此以外的搭配方式都会报错ValueError: Shape must be rank 2 but is rank 1 for 'decoder_layer1_18/MatMul' (op: 'MatMul') with input shapes: [4], [4,8].
+    除此以外的搭配方式都会报错
+        ValueError: Shape must be rank 2 but is rank 1 for 'decoder_layer1_18/MatMul' (op: 'MatMul') with input shapes: [4], [4,8].
     
     出错原因举个例子：
     def a():
@@ -225,7 +226,8 @@ def decoder(x):
     3
     (2, 3)
     
-    所以自编码器的程序中，如果encoder返回的是最后一个tensor，直接用x=tensor，再取x[-1]得到的将是tensor里的一个部分，而x=[tensor]，再取x[-1]得到的才是整个tensor。
+    所以自编码器的程序中，如果encoder返回的是最后一个tensor，直接用x=tensor，再取x[-1]得到的将是tensor里的一个部分，
+    而x=[tensor]，再取x[-1]得到的才是整个tensor。
     '''
     layers=[]
     layers.append(x)
